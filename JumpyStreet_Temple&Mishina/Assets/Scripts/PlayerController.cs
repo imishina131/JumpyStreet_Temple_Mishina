332ddd3c714497b89dbcfc52a0879b824f5f87a1
temple47 <37566259+temple47@users.noreply.github.com>
2026-01-13 18:25:18 -0500

    player controller

---
+++ JumpyStreet_Temple&Mishina/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float stepLength = 1f;
+
+    [SerializeField] float jumpHeight = 0.5f;
+    [SerializeField] float jumpTime = 0.25f;
+
+    private bool isMoving;
+
+    void Update()
+    {
+        if (isMoving || Keyboard.current == null) return;
+
+        // W
+        if (Keyboard.current.wKey.wasPressedThisFrame || Keyboard.current.upArrowKey.wasPressedThisFrame)
+            StartMove(Vector3.forward);
+
+        // S
+        if (Keyboard.current.sKey.wasPressedThisFrame || Keyboard.current.downArrowKey.wasPressedThisFrame)
+            StartMove(Vector3.back);
+
+        // A
+        if (Keyboard.current.aKey.wasPressedThisFrame || Keyboard.current.leftArrowKey.wasPressedThisFrame)
+            StartMove(Vector3.left);
+
+        // D
+        if (Keyboard.current.dKey.wasPressedThisFrame || Keyboard.current.rightArrowKey.wasPressedThisFrame)
+            StartMove(Vector3.right);
+    }
+
+    void StartMove(Vector3 dir)
+    {
+        // rotate
+        transform.forward = dir;
+
+        // jump
+        StartCoroutine(Jump(dir));
+    }
+
+    IEnumerator Jump(Vector3 dir)
+    {
+        isMoving = true;
+
+        // start position
+        Vector3 start = transform.position;
+
+        // end position stepLength away
+        Vector3 end = start + dir * stepLength;
+
+        // time
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / jumpTime;
+
+            // arc
+            float y = Mathf.Sin(t * Mathf.PI) * jumpHeight;
+
+            // move from start to end and jump
+            transform.position = Vector3.Lerp(start, end, t) + Vector3.up * y;
+
+            yield return null;
+        }
+
+        // move to end position
+        transform.position = end;
+        isMoving = false;
+    }
+}
