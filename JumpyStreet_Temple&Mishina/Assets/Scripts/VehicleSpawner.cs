95e5469209d3b12ab6367e8a0c0279f72f98930f
Irina Mishina <irinamishina131@gmail.com>
2026-01-21 16:18:09 -0500

    queen wont move??

--- JumpyStreet_Temple&Mishina/Assets/Scripts/VehicleSpawner.cs
+++ JumpyStreet_Temple&Mishina/Assets/Scripts/VehicleSpawner.cs
@@ -6,9 +6,13 @@ public class VehicleSpawner : MonoBehaviour
 
     [SerializeField] GameObject[] vehicles;
     [SerializeField] float minTime, maxTime, lifeTime, direction;
+    public GameObject targetObject;
+    Vector3 targetPosition;
+    GameObject prefabToSpawn;
 
     private void Start()
     {
+        targetPosition = targetObject.transform.position;
         StartCoroutine(SpawnVehicles());
     }
     IEnumerator SpawnVehicles()
@@ -19,14 +23,18 @@ public class VehicleSpawner : MonoBehaviour
             float delay = Random.Range(minTime, maxTime);
             yield return new WaitForSeconds(delay);
 
-            // spawn vehicle
-            int i = Random.Range(0, vehicles.Length);
-            GameObject vehicle = Instantiate(vehicles[i], transform.position, Quaternion.Euler(0f, direction, 0f));
-            vehicle.transform.Translate(Vector3.forward * 2.0f * Time.deltaTime);
-
+            // spawn prefab
+            prefabToSpawn = Resources.Load<GameObject>("Black Queen 1");
+            Instantiate(prefabToSpawn, new Vector3(transform.position.x, 0.2f, transform.position.z), Quaternion.Euler(0f, direction, 0f));
+            prefabToSpawn.transform.position = Vector3.MoveTowards(transform.position, targetPosition, 10.0f * Time.deltaTime);
             // destroy vehicle
-            Destroy(vehicle, lifeTime);
+            //Destroy(prefabToSpawn, lifeTime);
         }
+    }
+
+    void Update()
+    {
+        prefabToSpawn.transform.position = Vector3.MoveTowards(transform.position, targetPosition, 10.0f * Time.deltaTime);
     }
 
 }
